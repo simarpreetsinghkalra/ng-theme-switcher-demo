e4614fca16a012aa4d00933fa8ae88226a0a1b36
programmersingh <simarpreetsinghkalra@gmail.com>
2022-09-21 14:49:24 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-theme-switcher-demo';
+export class AppComponent implements OnInit {
+ isDarkTheme = false;
+
+ ngOnInit(): void {
+  const themeType = localStorage.getItem('themeType');
+  if(!themeType) {
+    this.isDarkTheme = false;
+    localStorage.setItem('themeType', 'light');
+    return;
+  }
+  this.isDarkTheme = themeType === 'dark';
+ }
+
+ switchTheme(): void {
+  this.isDarkTheme = !this.isDarkTheme;
+  const themeType = this.isDarkTheme ? 'dark' : 'light';
+  localStorage.setItem('themeType', themeType);
+ }
 }
